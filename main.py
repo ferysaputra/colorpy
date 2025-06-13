5dc3a47af7ffc74210dd42f438d47c61305b8e84
ferysaputra <ferycna@gmail.com>
2025-06-13 14:15:13 +0700

    color tambah wbg

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 import cv2
+import numpy as np
 
+# Open webcam
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+def get_color_name(h, s, v):
+    if s < 50 and v > 200:
+        return "WHITE"
+    elif v < 50:
+        return "BLACK"
+    elif s < 50:
+        return "GRAY"
+    
+    if 0 <= h < 10 or 170 <= h <= 180:
+        return "RED"
+    elif 10 <= h < 25:
+        return "ORANGE"
+    elif 25 <= h < 35:
+        return "YELLOW"
+    elif 35 <= h < 85:
+        return "GREEN"
+    elif 85 <= h < 125:
+        return "BLUE"
+    elif 125 <= h < 160:
+        return "VIOLET"
+    elif 160 <= h < 170:
+        return "PINK"
+    else:
+        return "UNDEFINED"
+
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
+    if not ret:
+        break
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     height, width, _ = frame.shape
-    
     cx = int(width / 2)
-    cy = int(height / 2) 
-
+    cy = int(height / 2)
 
     pixel_center = hsv_frame[cy, cx]
-    hue_value = pixel_center[0]
-    color = "Undefined"
-    if hue_value < 5:
-        color = "RED"
-    elif hue_value < 22:
-        color = "ORANGE"
-    elif hue_value < 33:
-        color = "YELLOW"
-    elif hue_value < 78:
-        color = "GREEN"
-    elif hue_value < 131:
-        color = "BLUE"
-    elif hue_value < 170:
-        color = "VIOLET"
-    else:
-        color = "RED"
+    h, s, v = int(pixel_center[0]), int(pixel_center[1]), int(pixel_center[2])
+    color = get_color_name(h, s, v)
 
-    pixel_center_bgr = frame[cy, cx]
-    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
-    
+    bgr_color = frame[cy, cx]
+    b, g, r = int(bgr_color[0]), int(bgr_color[1]), int(bgr_color[2])
+
     cv2.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
     cv2.putText(frame, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
     cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
@@ -43,11 +56,6 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break
-    
 
 cap.release()
-
-#while True:
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#       break
 cv2.destroyAllWindows()
